f1c7340d0ee29e345ee002c5ffb748216254edbd
ramzlab000 <ramzlab000@gmail.com>
2023-02-12 16:55:00 +0530

    minor style updates

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,9 +2,15 @@ import Link from "next/link"
 
 function Header() {
   return (
-    <header className="p-5 bg-blue-500">
+    <header className="flex gap-3 p-5 font-semibold bg-blue-500">
       <Link href="/" className="px-2 py-1 text-blue-500 bg-white rounded-lg">
         Home
+      </Link>
+      <Link href="/todos/" className="px-2 py-1 text-blue-500 bg-white rounded-lg">
+        TODOs
+      </Link>
+      <Link href="/search/" className="px-2 py-1 text-blue-500 bg-white rounded-lg">
+        Search
       </Link>
     </header>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <div>I am the home page</div>
+    <div className="w-full pt-4 text-4xl text-center">home page</div>
   )
 }
 export default Home
